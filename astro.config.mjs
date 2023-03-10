2eb1e2e9d3153ffbce5d3c28b2d12b5dbf175a98
Matas Mažeika <matas.mazeika@Hostingers-MacBook-Pro-3.local>
2023-03-10 19:28:16 +0400

    add laimejimu foto ir paslaugu info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import NetlifyCMS from 'astro-netlify-cms';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import NetlifyCMS from "astro-netlify-cms";
+import tailwind from "@astrojs/tailwind";
 
 import image from "@astrojs/image";
 import { astroImageTools } from "astro-imagetools";
@@ -12,107 +12,145 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), NetlifyCMS({
-    config: {
-      // Use Netlify’s “Git Gateway” authentication and target our default branch
-      backend: {
-        name: 'git-gateway',
-        branch: 'latest',
-        repo: 'matasmazeikaa/Tisperis'
+  integrations: [
+    tailwind(),
+    NetlifyCMS({
+      config: {
+        // Use Netlify’s “Git Gateway” authentication and target our default branch
+        backend: {
+          name: "git-gateway",
+          branch: "latest",
+          repo: "matasmazeikaa/Tisperis",
+        },
+        // Configure where our media assets are stored & served from
+        media_folder: "public/assets/media",
+        public_folder: "/assets/media",
+        // Configure the content collections
+        collections: [
+          {
+            name: "posts",
+            label: "Pagrindinis puslapis",
+            folder: "src/pages/posts",
+            fields: [
+              {
+                name: "title",
+                widget: "string",
+                label: "Hero sekcijos titulas",
+              },
+              {
+                name: "subtitle",
+                widget: "string",
+                label: "Hero sekcijos subtitulas",
+              },
+              {
+                name: "paslauga1Title",
+                widget: "string",
+                label: "Paslauga 1 titulas",
+              },
+              {
+                name: "paslauga2Title",
+                widget: "string",
+                label: "Paslauga 2 titulas",
+              },
+              {
+                name: "paslauga3Title",
+                widget: "string",
+                label: "Paslauga 3 titulas",
+              },
+              {
+                name: "paslauga1Info",
+                widget: "string",
+                label: "Paslauga 1 info",
+              },
+              {
+                name: "paslauga2Info",
+                widget: "string",
+                label: "Paslauga 2 info",
+              },
+              {
+                name: "paslauga3Info",
+                widget: "string",
+                label: "Paslauga 3 info",
+              },
+
+              {
+                name: "price2Weeks",
+                widget: "string",
+                label: "Kaina 2 savaites",
+              },
+
+              {
+                name: "price3Months",
+                widget: "string",
+                label: "Kaina 3 menesiai",
+              },
+
+              {
+                name: "price1Month",
+                widget: "string",
+                label: "Kaina 1 menesis",
+              },
+
+              {
+                name: "statistika",
+                widget: "list",
+                label: "Statistikos foto",
+                media_folder: "public/assets/media",
+                fields: [
+                  {
+                    name: "statistikosFoto",
+                    widget: "image",
+                    label: "Statistikos foto",
+                    allow_multiple: true,
+                    required: true,
+                  },
+                ],
+              },
+              {
+                name: "atsiliepimai",
+                widget: "list",
+                label: "Atsiliepimu foto",
+                media_folder: "public/assets/media",
+                fields: [
+                  {
+                    name: "atsiliepimaiFoto",
+                    widget: "image",
+                    label: "Atsiliepimu foto",
+                    allow_multiple: true,
+                    required: true,
+                  },
+                ],
+              },
+
+              {
+                name: "winnings",
+                widget: "list",
+                label: "Laimejimu foto",
+                media_folder: "public/assets/media",
+                fields: [
+                  {
+                    name: "winningFoto",
+                    widget: "image",
+                    label: "Laimejimo foto",
+                    allow_multiple: true,
+                    required: true,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
       },
-      // Configure where our media assets are stored & served from
-      media_folder: 'public/assets/media',
-      public_folder: '/assets/media',
-      // Configure the content collections
-      collections: [{
-        name: 'posts',
-        label: 'Pagrindinis puslapis',
-        folder: 'src/pages/posts',
-        fields: [{
-          name: 'title',
-          widget: 'string',
-          label: 'Hero sekcijos titulas'
-        }, {
-          name: 'subtitle',
-          widget: 'string',
-          label: 'Hero sekcijos subtitulas'
-        }, {
-          name: 'paslauga1Title',
-          widget: 'string',
-          label: 'Paslauga 1 titulas'
-        }, {
-          name: 'paslauga2Title',
-          widget: 'string',
-          label: 'Paslauga 2 titulas'
-        }, {
-          name: 'paslauga3Title',
-          widget: 'string',
-          label: 'Paslauga 3 titulas'
-        }, {
-          name: 'paslauga1Info',
-          widget: 'string',
-          label: 'Paslauga 1 info'
-        }, {
-          name: 'paslauga2Info',
-          widget: 'string',
-          label: 'Paslauga 2 info'
-        }, {
-          name: 'paslauga3Info',
-          widget: 'string',
-          label: 'Paslauga 3 info'
-        },
-        
-        {
-          name: 'price2Weeks',
-          widget: 'string',
-          label: 'Kaina 2 savaites'
-        },
-        
-        {
-          name: 'price3Months',
-          widget: 'string',
-          label: 'Kaina 3 menesiai'
-        },
-
-        {
-          name: 'price1Month',
-          widget: 'string',
-          label: 'Kaina 1 menesis'
-        },
-
-        {
-          name: 'statistika',
-          widget: 'list',
-          label: 'Statistikos foto',
-          media_folder: 'public/assets/media',
-          fields: [{
-            name: 'statistikosFoto',
-            widget: 'image',
-            label: 'Statistikos foto',
-            allow_multiple: true,
-            required: true
-          }]
-        }, {
-          name: 'atsiliepimai',
-          widget: 'list',
-          label: 'Atsiliepimu foto',
-          media_folder: 'public/assets/media',
-          fields: [{
-            name: 'atsiliepimaiFoto',
-            widget: 'image',
-            label: 'Atsiliepimu foto',
-            allow_multiple: true,
-            required: true
-          }]
-        }]
-      }]
-    },
-    previewStyles: ['/src/styles/global.css'],
-    disableIdentityWidgetInjection: true
-  }), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), astroImageTools, compress({
-    img: false,
-    svg: false
-  })]
+      previewStyles: ["/src/styles/global.css"],
+      disableIdentityWidgetInjection: true,
+    }),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    astroImageTools,
+    compress({
+      img: false,
+      svg: false,
+    }),
+  ],
 });
--- src/pages/posts/index.md
+++ src/pages/posts/index.md
@@ -6,12 +6,9 @@ subtitle: Naujienas, naujausius pasiūlymus, įvairias akcijas ir visą kitą
 paslauga1Title: Pilnai išanalizuoti statymai
 paslauga2Title: Klaidingos lažybų bendrovių linijos
 paslauga3Title: Pagalba klientams 24/7
-paslauga1Info: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat
-  nibh tristique ipsum.
-paslauga2Info: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat
-  nibh tristique ipsum.
-paslauga3Info: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat
-  nibh tristique ipsum.
+paslauga1Info: Visi musu komandos duodami statymai yra išanalizuoti ir argumentuoti
+paslauga2Info: Lažybų bendrovės neretai įvelia į pasiūlą klaidingų koeficientų, kuriais mes ir pasinaudojam :)
+paslauga3Info: Atsakysime į visus jums iškilusius klausimus
 price2Weeks: 69.99€
 price3Months: 249.99€
 price1Month: 119.99€
@@ -38,4 +35,90 @@ atsiliepimai:
   - atsiliepimaiFoto: /assets/media/image00019.jpeg
   - atsiliepimaiFoto: /assets/media/image00021.jpeg
   - atsiliepimaiFoto: /assets/media/image00022.jpeg
+winnings:
+  - winningFoto: /assets/media/laimejimas0.jpeg
+  - winningFoto: /assets/media/laimejimas1.jpeg
+  - winningFoto: /assets/media/laimejimas2.jpeg
+  - winningFoto: /assets/media/laimejimas3.jpeg
+  - winningFoto: /assets/media/laimejimas4.jpeg
+  - winningFoto: /assets/media/laimejimas5.jpeg
+  - winningFoto: /assets/media/laimejimas6.jpeg
+  - winningFoto: /assets/media/laimejimas7.jpeg
+  - winningFoto: /assets/media/laimejimas8.jpeg
+  - winningFoto: /assets/media/laimejimas9.jpeg
+  - winningFoto: /assets/media/laimejimas10.jpeg
+  - winningFoto: /assets/media/laimejimas11.jpeg
+  - winningFoto: /assets/media/laimejimas12.jpeg
+  - winningFoto: /assets/media/laimejimas13.jpeg
+  - winningFoto: /assets/media/laimejimas14.jpeg
+  - winningFoto: /assets/media/laimejimas15.jpeg
+  - winningFoto: /assets/media/laimejimas16.jpeg
+  - winningFoto: /assets/media/laimejimas17.jpeg
+  - winningFoto: /assets/media/laimejimas18.jpeg
+  - winningFoto: /assets/media/laimejimas19.jpeg
+  - winningFoto: /assets/media/laimejimas20.jpeg
+  - winningFoto: /assets/media/laimejimas21.jpeg
+  - winningFoto: /assets/media/laimejimas22.jpeg
+  - winningFoto: /assets/media/laimejimas23.jpeg
+  - winningFoto: /assets/media/laimejimas24.jpeg
+  - winningFoto: /assets/media/laimejimas25.jpeg
+  - winningFoto: /assets/media/laimejimas26.jpeg
+  - winningFoto: /assets/media/laimejimas27.jpeg
+  - winningFoto: /assets/media/laimejimas28.jpeg
+  - winningFoto: /assets/media/laimejimas29.jpeg
+  - winningFoto: /assets/media/laimejimas30.jpeg
+  - winningFoto: /assets/media/laimejimas31.jpeg
+  - winningFoto: /assets/media/laimejimas32.jpeg
+  - winningFoto: /assets/media/laimejimas33.jpeg
+  - winningFoto: /assets/media/laimejimas34.jpeg
+  - winningFoto: /assets/media/laimejimas35.jpeg
+  - winningFoto: /assets/media/laimejimas36.jpeg
+  - winningFoto: /assets/media/laimejimas37.jpeg
+  - winningFoto: /assets/media/laimejimas38.jpeg
+  - winningFoto: /assets/media/laimejimas39.jpeg
+  - winningFoto: /assets/media/laimejimas40.jpeg
+  - winningFoto: /assets/media/laimejimas41.jpeg
+  - winningFoto: /assets/media/laimejimas42.jpeg
+  - winningFoto: /assets/media/laimejimas43.jpeg
+  - winningFoto: /assets/media/laimejimas44.jpeg
+  - winningFoto: /assets/media/laimejimas45.jpeg
+  - winningFoto: /assets/media/laimejimas46.jpeg
+  - winningFoto: /assets/media/laimejimas47.jpeg
+  - winningFoto: /assets/media/laimejimas48.jpeg
+  - winningFoto: /assets/media/laimejimas49.jpeg
+  - winningFoto: /assets/media/laimejimas50.jpeg
+  - winningFoto: /assets/media/laimejimas51.jpeg
+  - winningFoto: /assets/media/laimejimas52.jpeg
+  - winningFoto: /assets/media/laimejimas53.jpeg
+  - winningFoto: /assets/media/laimejimas54.jpeg
+  - winningFoto: /assets/media/laimejimas55.jpeg
+  - winningFoto: /assets/media/laimejimas56.jpeg
+  - winningFoto: /assets/media/laimejimas57.jpeg
+  - winningFoto: /assets/media/laimejimas58.jpeg
+  - winningFoto: /assets/media/laimejimas59.jpeg
+  - winningFoto: /assets/media/laimejimas60.jpeg
+  - winningFoto: /assets/media/laimejimas61.jpeg
+  - winningFoto: /assets/media/laimejimas62.jpeg
+  - winningFoto: /assets/media/laimejimas63.jpeg
+  - winningFoto: /assets/media/laimejimas64.jpeg
+  - winningFoto: /assets/media/laimejimas65.jpeg
+  - winningFoto: /assets/media/laimejimas66.jpeg
+  - winningFoto: /assets/media/laimejimas67.jpeg
+  - winningFoto: /assets/media/laimejimas68.jpeg
+  - winningFoto: /assets/media/laimejimas69.jpeg
+  - winningFoto: /assets/media/laimejimas70.jpeg
+  - winningFoto: /assets/media/laimejimas71.jpeg
+  - winningFoto: /assets/media/laimejimas72.jpeg
+  - winningFoto: /assets/media/laimejimas73.jpeg
+  - winningFoto: /assets/media/laimejimas74.jpeg
+  - winningFoto: /assets/media/laimejimas75.jpeg
+  - winningFoto: /assets/media/laimejimas76.jpeg
+  - winningFoto: /assets/media/laimejimas77.jpeg
+  - winningFoto: /assets/media/laimejimas78.jpeg
+  - winningFoto: /assets/media/laimejimas79.jpeg
+  - winningFoto: /assets/media/laimejimas80.jpeg
+  - winningFoto: /assets/media/laimejimas81.jpeg
+  - winningFoto: /assets/media/laimejimas82.jpeg
+  - winningFoto: /assets/media/laimejimas83.jpeg
+  - winningFoto: /assets/media/laimejimas84.jpeg
 ---
